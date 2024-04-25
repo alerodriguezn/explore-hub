5c600250636dcbc517fc1148359f59b00e5739d0
Esteban Salazar <68300629+Estebans441@users.noreply.github.com>
2024-04-25 01:16:59 +0000

    feat: initial transport page

---
+++ src/app/(home)/cars/page.tsx
@@ -0,0 +1,9 @@
+import TransportCard from "@/components/accommodation/TransportCard";
+
+export default function AccommodationPage() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center '>   
+        <TransportCard/>
+    </div>
+  );
+}
