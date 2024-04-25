fcd97adec3b155588c2d512b50ece3e90b16fe2c
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-04-25 01:25:27 -0500

    fix: icons and transport directory

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -2,5 +2,8 @@ import StarIcon from '@mui/icons-material/Star';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
 import PeopleIcon from '@mui/icons-material/People';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 
-export { StarIcon, SettingsIcon, LocalGasStationIcon, PeopleIcon };
+export {StarIcon, SettingsIcon, LocalGasStationIcon, PeopleIcon, BeachAccessIcon, ApartmentIcon, DirectionsCarIcon}
